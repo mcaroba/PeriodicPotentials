f76d0aee5c5285a0ee10176207b742d3ee58f2fe
Thomas Pfau <github@thomaspfau.de>
2022-10-14 12:19:57 +0300

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div style="width: 20%; float:right">
+    <h2> Available Potentials </h2>
+    <table style="width:100%">
+      <tr v-if="availablePotentials.length > 0">
+      <td><b> Potential Name</b> </td><td><b>Elements</b></td></tr>
+      <tr v-for ="(potential,key) in availablePotentials" :key="key">
+        <td @click="displayDetails(potential)"><u>{{potential.name}}</u></td> <td>{{potential.elements}}</td>
+      </tr>
+    </table> 
+  </div>
+  <div style="width: 80%">
+      <PeriodicTable
+      @selectionChanged="(element) => updateSelection(element.element, element.selected)"/>
+  </div>
+    <!-- use the modal component, pass in the prop -->
+    <PotentialDetailModal :show="showModal" :potentialDetails="currentPotential" @close="showModal = false" />      
+</template>
+
+<script>
+import PeriodicTable from './components/PeriodicTable.vue'
+import potentials from './assets/potentials.json'
+import PotentialDetailModal from './components/PotentialDetailModal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PeriodicTable,
+    PotentialDetailModal    
+  },
+  data ()
+  {
+    return {
+      selectedElements: [],
+      currentPotential: {},
+      showModal: false
+    }
+  },
+  methods:
+  {
+    updateSelection(element, added)
+    {
+      if(added)
+      {
+        this.selectedElements.push(element.small);
+      }
+      else{
+        var index = this.selectedElements.indexOf(element.small);
+        if (index !== -1) {
+          this.selectedElements.splice(index, 1);
+        }
+      }
+    },
+    displayDetails(potential)
+    {
+      console.log(potential)
+      this.currentPotential = potential;      
+      this.showModal = true;
+    },  
+  },
+  computed:
+  {
+    availablePotentials()
+    {
+      const available = []
+      for(const potential of potentials)
+      {
+        if(this.selectedElements.every((x) => potential.elements.includes(x)))
+        {
+          available.push(potential);
+        }
+      }
+      return available;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+
+</style>
